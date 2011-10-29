6f4f9ebeee6c7ccd60d27eebc54e8a64dd2675f9
oranie <t.narita0917+jibun@gmail.com>
2011-10-29 20:05:37 +0900

    erase

--- dump.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mysqldump -uroot -pyura0244 -h  ip-10-148-126-202.ap-northeast-1.compute.internal --default-character-set=utf8 --hex-blob slow_log log_table_history LOG_ANALYZE_RESOURCE LOG_ANALYZE_TOTAL_EVERY_TIME > dump.sql
-#mysqldump -uroot -pyura0244  --default-character-set=utf8 --hex-blob slow_log log_table_history LOG_ANALYZE_RESOURCE LOG_ANALYZE_TOTAL_EVERY_TIME > dump.sql
